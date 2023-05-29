773fa72d3db33c6e6a5403357839bc506e278f4c
Carrie <carrie.murchison@thelastmile.org>
2023-05-29 12:55:27 -0700

    update to use index.html

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const PORT = process.env.PORT || 3000;
 
 app.use("/api/quotes",require('./routes/quotes'));
 
+// Define the static file directory
+app.use(express.static(__dirname));
+
+// Define a route to serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Listening On port ${PORT}`);
 });
